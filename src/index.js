7aaca15bef383aa1a1df1276a3f450082ff33df0
ntokozo-s <ntokozomokoena655@gmail.com>
2024-12-09 00:43:27 +0200

    Added AI API intergration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePrompt(event) {
-  event.preventDefault();
-
+function displayPrompt(response) {
   new Typewriter("#story-prompt", {
-    strings: "A prince has choose between the crown and his true love.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePrompt(event) {
+  event.preventDefault();
+
+  let promptGeneratorInput = document.querySelector("#user-instructions");
+  let apiKey = "8tca415f35dad4af35ac2o5bfe7061a9";
+  let context =
+    "You are a literature expert who enjoys writing fictional short-stories and has expertise in creating story writing prompts. Your mission is to generate a short-story prompt that is at least two to three sentences long at most. keep the sentences short and concise and display the prompt in basic HTML and separate each sentence with a <br />. Make sure to follow the user instructions. Do not include a title to the short-story.";
+  let prompt = `User instructions: Generate a short-story prompt about ${promptGeneratorInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPrompt);
 }
 
 let promptForm = document.querySelector("#story-prompt-form");
