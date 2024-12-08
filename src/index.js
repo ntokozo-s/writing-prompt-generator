638008a5c41daf3712cf4a80c0e3c60c537df526
ntokozo-s <ntokozomokoena655@gmail.com>
2024-12-08 23:32:46 +0200

    Added basic form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePrompt(event) {
+  event.preventDefault();
 
+  new Typewriter("#story-prompt", {
+    strings: "A prince has choose between the crown and his true love.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let promptForm = document.querySelector("#story-prompt-form");
+promptForm.addEventListener("submit", generatePrompt);
